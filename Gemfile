7abd869cda6722355bad7b5aa304387e2791f8cb
flemdizzle <fleminsh@gmail.com>
2014-10-27 15:32:39 -0400

    made if so the create a user profile functionality works in the views

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'pry', group: :development
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.6'
 # Use postgres as the database for Active Record
-gem 'pg'
+gem 'pg' 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ActiveRecord::Base
     validates :name,
       presence: true,
       length: { maximum: 50 }
+
     validates :email,
       presence: true,
       uniqueness: { case_sensitive: false },
-      formate: { with: /[^@]+@[^@]+/ }
+      format: { with: /[^@]+@[^@]+/ }
+
     validates :password,
       length: { minimum: 8 }
 
