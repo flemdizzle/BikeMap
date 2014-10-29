a605b7a53a89a9c83d237b411eb525748df395ff
flemdizzle <fleminsh@gmail.com>
2014-10-29 17:50:46 -0400

    got the basic modal working and the closest station finder

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -7,5 +7,6 @@ class TripsController < ApplicationController
       @cbs = params[:cbs]
       @start_stations = Station.return_3_closest_stations(@start)
       @end_stations = Station.return_3_closest_stations(@end)
+      @bikes_available = Station.available_bikes(25, 2, 9)
     end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,13 +1,20 @@
 class Station < ActiveRecord::Base
+   geocoded_by :location
+   require 'open-uri'
 
    def self.return_3_closest_stations(location)
-      geocoded_location = Geocoder.coordinates(location)
-      array = []
-      Station.all.each do |x|
-         binding.pry
-         array << distance_between(geocoded_location, [x.latitude, x.longitude])
+      lat_long_location = Geocoder.coordinates(location)
+      near = Station.near(location).first(3)
+   end
+
+   def self.available_bikes(distance, id)
+      doc = Nokogiri::XML(open("http://www.capitalbikeshare.com/data/stations/bikeStations.xml"))
+      # spits the number of bikes at a given station
+      doc.xpath('//station').each do |node|
+         if node.children[0].text == id
+            array << "#{node.children[1].text} Station #{node.children[12].text}/#{node.children[12].text.to_i + node.children[13].text.to_i} bikes available."
+         end
       end
-      array
    end
 
    def self.update_station_info
