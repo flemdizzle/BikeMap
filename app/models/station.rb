7a626988a4c668d793048a1e6fdd96e86f1ff2c9
flemdizzle <fleminsh@gmail.com>
2014-10-30 10:41:13 -0400

    figured out how to add waypoints, starting to fix what the station controller is passing to the view

--- app/models/station.rb
+++ app/models/station.rb
@@ -3,16 +3,21 @@ class Station < ActiveRecord::Base
    require 'open-uri'
 
    def self.return_3_closest_stations(location)
+      near_station_array = []
       lat_long_location = Geocoder.coordinates(location)
       near = Station.near(location).first(3)
+      near.each do |station|
+         near_station_array << Station.available_bikes(station.distance, station.station_id)
+      end
+      near_station_array
    end
 
    def self.available_bikes(distance, id)
       doc = Nokogiri::XML(open("http://www.capitalbikeshare.com/data/stations/bikeStations.xml"))
-      # spits the number of bikes at a given station
+      # spits out the number of bikes at a given station
       doc.xpath('//station').each do |node|
-         if node.children[0].text == id
-            array << "#{node.children[1].text} Station #{node.children[12].text}/#{node.children[12].text.to_i + node.children[13].text.to_i} bikes available."
+         if node.children[0].text == "#{id}"
+            return "#{distance.round(2)}mi. #{node.children[1].text} Station #{node.children[12].text}/#{node.children[12].text.to_i + node.children[13].text.to_i} bikes."
          end
       end
    end
