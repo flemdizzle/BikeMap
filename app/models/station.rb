5e4178610d719b1d0815169a4011e8e2e727e89f
flemdizzle <fleminsh@gmail.com>
2015-01-05 19:01:38 -0500

    got full test coverage on the stations model, but the tests are pretty terrible

--- app/models/station.rb
+++ app/models/station.rb
@@ -12,7 +12,7 @@ class Station < ActiveRecord::Base
 
    def self.return_3_closest_stations(location)
       near_station_array = []
-      lat_long_location = Geocoder.coordinates(location)
+      # lat_long_location = Geocoder.coordinates(location)
       near = Station.near(location).first(3)
       near.each do |station|
          near_station_array << Station.available_bikes(station.distance, station.station_id)
@@ -37,13 +37,13 @@ class Station < ActiveRecord::Base
       end
    end
 
-   def self.update_station_info
-      Station.delete_all
-      doc = Nokogiri::XML(open("http://www.capitalbikeshare.com/data/stations/bikeStations.xml"))
-      doc.xpath('//station').each do |node|
-         Station.create(station_id: node.children[0].text.to_i,
-            latitude: node.children[4].text.to_f,
-            longitude: node.children[5].text.to_f)
-      end
-   end
+   # def self.update_station_info
+   #    Station.delete_all
+   #    doc = Nokogiri::XML(open("http://www.capitalbikeshare.com/data/stations/bikeStations.xml"))
+   #    doc.xpath('//station').each do |node|
+   #       Station.create(station_id: node.children[0].text.to_i,
+   #          latitude: node.children[4].text.to_f,
+   #          longitude: node.children[5].text.to_f)
+   #    end
+   # end
 end
--- test/models/stations_test.rb
+++ test/models/stations_test.rb
@@ -3,7 +3,14 @@ require 'test_helper'
 class StationsTest < ActiveSupport::TestCase
 
   test "lat_long returns array of station lat longs" do 
-    assert_equal(Array, Station.lat_long.class)
+    assert_kind_of(Array, Station.lat_long)
   end
 
+  test "return_3_closest_stations returns an array" do 
+    assert_kind_of(Array, Station.return_3_closest_stations(-77.0512))
+  end
+
+  test "available_bikes returns a hash" do 
+    assert_kind_of(Hash, Station.available_bikes(10, 5))
+  end
 end
